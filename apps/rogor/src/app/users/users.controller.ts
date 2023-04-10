9844f84ffd10c4d4d7e6591fb1b9455bc73393db
Farukh Fuzaylov <fuzailof@gmail.com>
2023-04-10 11:11:26 +0400

    feature: add pagination for users

--- apps/rogor/src/app/users/users.controller.ts
+++ apps/rogor/src/app/users/users.controller.ts
@@ -6,23 +6,25 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PaginationQueryDto } from '../../common/dto/pagination-query.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get()
+  findAll(@Query() paginationQueryDto: PaginationQueryDto) {
+    return this.usersService.findAll(paginationQueryDto);
+  }
+
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
   }
 
   @Get(':id')
---
+++ apps/rogor/src/common/dto/pagination-query.dto.spec.ts
@@ -0,0 +1,7 @@
+import { PaginationQueryDto } from './pagination-query.dto';
+
+describe('PaginationQueryDto', () => {
+  it('should be defined', () => {
+    expect(new PaginationQueryDto()).toBeDefined();
+  });
+});
