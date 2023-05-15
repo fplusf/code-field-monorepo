028e0a003f23088f35ed473023e8448ef69d7ba0
Farukh Fuzaylov <fuzailof@gmail.com>
2023-05-15 22:32:52 +0400

    feat(client/auth) impelement basic sing in & sign up using React

--- apps/client/auth/src/app/components/Login.tsx
+++ apps/client/auth/src/app/components/Login.tsx
@@ -1,10 +1,18 @@
-import { Alert, Button, Stack, TextField } from '@mui/material';
-import React from 'react';
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Stack,
+  TextField,
+} from '@mui/material';
+import React, { useState } from 'react';
 // import { useStore } from '@ngneat/elf';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { SignInFormType, signInFormSchema } from '../models/sign-in.model';
+import { useHistory } from 'react-router-dom';
 
 const Login: React.FC = () => {
   const {
@@ -15,8 +23,43 @@ const Login: React.FC = () => {
     resolver: zodResolver(signInFormSchema),
   });
 
-  const onSubmit = (data: SignInFormType) => {
+  const [apiError, setApiError] = useState('');
+  const history = useHistory();
+
+  const onSubmit = async (data: SignInFormType) => {
     console.log('data', data);
+
+    const url =
+      import.meta.env.MODE === 'development'
+        ? import.meta.env.VITE_API_URL + '/authentication/sign-in'
+        : import.meta.env.VITE_PROD_API_URL + '/authentication/sign-in';
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.status === 200) {
+      const token = await response.json();
+      console.log('Login successful');
+
+      // TODO: Handle it in a better way
+      localStorage.setItem('accessToken', token.accessToken);
+      localStorage.setItem('refreshToken', token.refreshToken);
+
+      setApiError('');
+
+      // redirect to home page
+      history.push('/');
+    } else {
+      // Login failed, handle error (e.g. display error message)
+      const error = await response.json();
+      console.error('Login failed: ', error);
+      setApiError(error.message);
+    }
   };
 
   return (
@@ -47,16 +90,22 @@ const Login: React.FC = () => {
         {...register('password')}
       />
 
-      <Button type="submit" variant="contained" color="primary">
-        Login
-      </Button>
+      {isSubmitting ? (
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <CircularProgress style={{ height: '36.5px' }} />
+        </Box>
+      ) : (
+        <Button type="submit" variant="contained" color="primary">
+          Login
+        </Button>
+      )}
 
       <div style={{ height: '3rem' }}>
-        {!!Object.keys(errors).length && (
+        {errors.email?.message || errors.password?.message || apiError ? (
           <Alert severity="error">
-            {errors.email?.message || errors.password?.message}
+            {errors.email?.message || errors.password?.message || apiError}
           </Alert>
-        )}
+        ) : null}
       </div>
 
       <p className="text-center">Don't have an account?</p>
--- apps/client/auth/src/app/components/Signup.tsx
+++ apps/client/auth/src/app/components/Signup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TextField,
   Button,
@@ -6,6 +6,8 @@ import {
   Checkbox,
   Alert,
   FormControlLabel,
+  Box,
+  CircularProgress,
 } from '@mui/material';
 // import authStore from '../store';
 // import { useStore } from '@ngneat/elf';
@@ -13,6 +15,7 @@ import { Link } from 'react-router-dom';
 import { SignUpFormType, signUpformSchema } from '../models/sign-up.model';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useHistory } from 'react-router-dom';
 
 const Signup: React.FC = () => {
   const {
@@ -23,8 +26,48 @@ const Signup: React.FC = () => {
     resolver: zodResolver(signUpformSchema),
   });
 
-  const onSubmit: SubmitHandler<SignUpFormType> = (data) => {
+  const [apiError, setApiError] = useState('');
+  const history = useHistory();
+
+  const onSubmit: SubmitHandler<SignUpFormType> = async (data) => {
     console.log(data);
+
+    // TODO: Remove this once we have confirmPassword & terms in BE.
+    const validData = {
+      username: data.username,
+      email: data.email,
+      password: data.password,
+      firstName: 'John',
+      lastName: 'Doe',
+    };
+
+    const url =
+      import.meta.env.MODE === 'development'
+        ? import.meta.env.VITE_API_URL + '/authentication/sign-up'
+        : import.meta.env.VITE_PROD_API_URL + '/authentication/sign-up';
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(validData),
+    });
+
+    if (response.status === 201) {
+      // Login successful, do something here (e.g. redirect to home page)
+      console.log('Login successful: ', await response.json());
+      setApiError('');
+      // redirect to login page
+      history.push('/login');
+    } else {
+      // Login failed, handle error (e.g. display error message)
+      const error = await response.json();
+      console.error('Login failed: ', error);
+      setApiError(error.message);
+
+      console.log('apiError', apiError);
+    }
   };
 
   return (
@@ -67,24 +110,37 @@ const Signup: React.FC = () => {
           </Link>
         }
       />
-      <Button
-        disabled={isSubmitting}
-        type="submit"
-        variant="contained"
-        color="primary"
-      >
-        Signup
-      </Button>
+
+      {isSubmitting ? (
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <CircularProgress style={{ height: '36.5px' }} />
+        </Box>
+      ) : (
+        <Button
+          disabled={isSubmitting}
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Signup
+        </Button>
+      )}
+
       <div style={{ height: '3rem' }}>
-        {!!Object.keys(errors).length && (
-          <Alert severity="error">
-            {errors.username?.message ||
-              errors.email?.message ||
-              errors.password?.message ||
-              errors.confirmPassword?.message ||
-              errors.terms?.message}
-          </Alert>
-        )}
+        {[
+          errors.username?.message,
+          errors.email?.message,
+          errors.password?.message,
+          errors.confirmPassword?.message,
+          errors.terms?.message,
+          apiError,
+        ]
+          .filter(Boolean)
+          .map((errorMessage, index) => (
+            <Alert key={index} severity="error">
+              {errorMessage}
+            </Alert>
+          ))}
       </div>
 
       <p>Already have an account?</p>
